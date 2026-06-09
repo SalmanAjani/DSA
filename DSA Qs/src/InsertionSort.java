84bf660a196f51c795b756335799c921cbf1ea80
Salman <salmanajani0426@gmail.com>
2026-06-09 19:19:07 +0530

    Base insertion sort implementation

---
+++ DSA Qs/src/InsertionSort.java
@@ -0,0 +1,32 @@
+// Time Complexity - O(n²)
+
+public class InsertionSort {
+    static void main(String[] args) {
+        int arr[] = {5,6,2,3,1,8,4};
+        int size = arr.length;
+
+        System.out.println("Before sorting");
+        for(int num : arr){
+            System.out.print(num + " ");
+        }
+
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i-1;
+
+            while(j>=0 && arr[j] > key){
+                arr[j+1] = arr[j];
+                j--;
+            }
+
+            arr[j+1] = key;
+        }
+
+        System.out.println();
+        System.out.println();
+        System.out.println("After sorting");
+        for(int num : arr){
+            System.out.print(num + " ");
+        }
+    }
+}
